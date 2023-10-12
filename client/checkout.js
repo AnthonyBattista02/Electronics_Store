5d74e8d7b4c79804a5a111741f4d884b2317f4a2
SimealWoldu <swoldu01@gmail.com>
2023-10-12 12:48:07 -0400

    Update remove function

--- client/checkout.js
+++ client/checkout.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function() {
+    await getProducts()
     const cartItemsContainer = document.getElementById('cartItems');
     const subtotalContainer = document.getElementById('subtotal');
     
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         const response = await axios.get(`http://localhost:3001/products/${productId}`);
         const product = response.data;
         const productDiv = document.createElement('div');
+        productDiv.setAttribute('data-product-id', product._id);
         productDiv.innerHTML = `
         <img id="productImage" src="${product.imageURL}" alt="${product.name}">
         <h2 id="productName">${product.name}</h2>
@@ -24,12 +26,48 @@ document.addEventListener('DOMContentLoaded', async function() {
 });
 
 function removeFromCart(productId) {
-    // Get the current cart from local storage
+
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    // Remove the product ID from the cart array
-    cart = cart.filter(id => id !== productId);
+
+    // Find the index of the product ID in the cart array
+    const index = cart.indexOf(productId);
+
+    // If the product ID is found, remove it from the cart array
+    if (index > -1) {
+        cart.splice(index, 1);
+    }
     // Update the cart in local storage
     localStorage.setItem('cart', JSON.stringify(cart));
-    // Refresh the page to reflect the changes (you can also manually update the DOM if you prefer not to refresh)
-    location.reload();
+
+    // Remove the product div from the cartItemsContainer
+    const productDiv = document.querySelector(`div[data-product-id="${productId}"]`);
+    if (productDiv) {
+        productDiv.remove();
+    }
+
+    updateSubtotal();
+}
+
+//Global products setup
+let products = [];
+async function getProducts() {
+    const response = await axios.get('http://localhost:3001/products');
+    products = response.data;
+    console.log(products);
+    return products;
+}
+
+
+function updateSubtotal() {
+    let subtotal = 0;
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    for (let productId of cart) {
+        // Assuming you have a global products array or similar
+        const product = products.find(p => p._id === productId);
+        if (product) {
+            subtotal += parseFloat(product.price);
+        }
+    }
+    const subtotalContainer = document.getElementById('subtotal');
+    subtotalContainer.innerHTML = `Subtotal: $${subtotal.toFixed(2)}`;
 }
